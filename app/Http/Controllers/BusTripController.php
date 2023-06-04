87620e1ff2df6fea1f620b8ca33a3f51f9a0e355
Mahmoud Bahaa <mbahaa1997@hotmail.com>
2023-06-04 15:23:28 +0300

    [Ref]

--- app/Http/Controllers/BusTripController.php
+++ app/Http/Controllers/BusTripController.php
@@ -6,26 +6,36 @@ use App\Http\Requests\CreateTripRequest;
 use App\Http\Requests\RemoveTripRequest;
 use App\Models\BusTrip;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class BusTripController extends Controller
 {
-    public function add(CreateTripRequest $request):JsonResponse
+    public function add(CreateTripRequest $request): JsonResponse
     {
         $input = $request->all();
-        $trip = BusTrip::create($input);
 
+        $trip = DB::table('bus_trips')
+            ->where('name', $input['name'])
+            ->first();
+        if ($trip != null) {
+            return response()->json([
+                'message' => 'Bus trip with name already exists',
+                'station' => $trip
+            ])->setStatusCode(400);
+        }
+
+        $trip = BusTrip::create($input);
         return response()->json([
             'message' => 'Trip created successfully',
             'trip' => $trip
         ]);
     }
 
-    public function remove(RemoveTripRequest $request):JsonResponse
+    public function remove(RemoveTripRequest $request): JsonResponse
     {
         $input = $request->all();
         $trip = BusTrip::find($input['id'])->delete();
-
         return response()->json([
             'message' => 'Trip deleted successfully'
         ]);
--- app/Http/Controllers/TripStationController.php
+++ app/Http/Controllers/TripStationController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\CreateTripStationRequest;
 use App\Http\Requests\RemoveTripStationRequest;
 use App\Models\TripStation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TripStationController extends Controller
@@ -13,6 +14,17 @@ class TripStationController extends Controller
     public function add(CreateTripStationRequest $request):JsonResponse
     {
         $input = $request->all();
+        $tripStation = DB::table('trip_stations')
+        ->where('trip_id', $input['trip_id'])
+        ->where('station_id', $input['station_id'])
+        ->first();
+        if($tripStation != null)
+        {
+            return response()->json([
+                'message' => 'Trip Station already exists',
+                'station' => $tripStation
+            ])->setStatusCode(400);
+        }
         $tripStation = TripStation::create($input);
 
         return response()->json([
@@ -24,8 +36,13 @@ class TripStationController extends Controller
     public function remove(RemoveTripStationRequest $request):JsonResponse
     {
         $input = $request->all();
-        $tripStation = TripStation::find($input['id'])->delete();
-
+        $tripStation = TripStation::find($input['id']);
+        if ($tripStation == null) {
+            return response()->json([
+                'message' => 'Trip station does not exist.'
+            ])->setStatusCode(400);
+        }
+        $tripStation->delete();
         return response()->json([
             'message' => 'Trip Station deleted successfully'
         ]);
